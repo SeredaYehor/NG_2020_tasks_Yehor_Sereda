c64696af5aff62127e538a360a05ddb30f16c125
Sirjeleshka <74448293+Sirjeleshka@users.noreply.github.com>
2020-11-14 15:52:52 +0200

    Added #7

--- NG_2020_task7/main.cpp
+++ NG_2020_task7/main.cpp
@@ -7,6 +7,10 @@ int main()
     int x,y;
     cout << "Vvedite chislo:" << endl;
     cin >> x;
-    for(y=0;y<=x;y++) cout << y << " ";
+    for(y=0;y<=x;y++)
+    {
+        cout << y;
+        if(y<x) cout << ',';
+    }
     return 0;
 }
