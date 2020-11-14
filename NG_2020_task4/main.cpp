717decb36e389a624549dc6ef7150ccfd620298b
Sirjeleshka <74448293+Sirjeleshka@users.noreply.github.com>
2020-11-14 15:48:45 +0200

    Added #4

--- NG_2020_task4/main.cpp
+++ NG_2020_task4/main.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int main()
 {
     int income;
+    float million = 1000000.1;
     cout << "Skolko ti zarabativaesh?" << endl;
     cin >> income;
-    if (income < 1000000)
+    if (income < million)
        {
-         if ( income > 1000) cout <<  "Tak derzhat!";
+         if (income > 1000) cout <<  "Tak derzhat!";
          if (income < 1000) cout << "Rabotai bolshe";
-
        }
-    if (income > 1000000)
+    if (income > million)
        {
         cout << "Ti millioner!";
        }
