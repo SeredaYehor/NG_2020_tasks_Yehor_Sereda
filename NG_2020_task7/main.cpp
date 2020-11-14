f851c851904df284f621e75a0ab5205a17f29cad
Sirjeleshka <74448293+Sirjeleshka@users.noreply.github.com>
2020-11-14 13:10:37 +0200

    Added #7

---
+++ NG_2020_task7/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cout << "Vvedite chislo:" << endl;
+    cin >> x;
+    for(y=0;y<=x;y++) cout << y << " ";
+    return 0;
+}
