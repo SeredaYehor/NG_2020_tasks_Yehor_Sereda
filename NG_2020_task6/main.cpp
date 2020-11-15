8b2235e0fbbecc0d37066f5f1d0167ea107ffa3a
Sirjeleshka <sirjeleshka@gmail.com>
2020-11-15 11:48:11 +0200

    Added #6

--- NG_2020_task6/main.cpp
+++ NG_2020_task6/main.cpp
@@ -4,51 +4,46 @@ using namespace std;
 
 int main()
 {
-    int x,symbol,stroka,i,stroka2,white_space;
+    int x,stroka,y;
     cout << "Enter size:";
     cin >> x;
-    i=x-(x-1); //stroka-1
-    stroka2=x;
     for (stroka=1; stroka <= x; stroka++)
         {
-        for (symbol=1;symbol<= x;symbol++) cout << '*';
+        for (y=1;y <= x;y++) cout << '*';
         cout << endl;
         }
     cout << endl;
-    for (stroka=1; stroka <= x; stroka++)
+    for (stroka=0; stroka < x;)
         {
-          for(symbol=0;symbol<i;symbol++) cout << '*';
-          i++;
+          for(y=0;y<=stroka;y++) cout << '*';
+          stroka++;
           cout << endl;
         }
     cout << endl;
-    white_space=x;
     for(stroka=0;stroka<x;stroka++)
     {
-        for(symbol=1;symbol<=x;symbol++)
+        for(y=1;y<=x;y++)
         {
-            if (symbol<white_space) cout << " ";
+            if (y<x-stroka) cout << " ";
             else cout << '*';
         }
-        white_space--;
         cout << endl;
     }
     cout << endl;
     for (stroka=1; stroka <= x; stroka++)
         {
-            for(symbol=0;symbol<stroka2;symbol++) cout << '*';
-            stroka2--;
+            for(y=0;y<=x-stroka;y++) cout << '*';
             cout << endl;
         }
     cout << endl;
-    for(stroka=0;stroka<x;stroka++)
+    for(stroka=0;stroka<x;)
     {
-        for(symbol=1;symbol<=x&&white_space<x;symbol++)
+        for(y=1;y<=x;y++)
         {
-            if (symbol<=white_space) cout << " ";
+            if (y<=stroka) cout << " ";
             else cout << '*';
         }
-        white_space++;
+        stroka++;
         cout << endl;
     }
     return 0;
