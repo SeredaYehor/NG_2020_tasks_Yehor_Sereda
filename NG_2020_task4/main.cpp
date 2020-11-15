5da18c13724cfae5916fa2cbe47410175a0305e7
Sirjeleshka <sirjeleshka@gmail.com>
2020-11-15 14:08:01 +0200

    Added #4

--- NG_2020_task4/main.cpp
+++ NG_2020_task4/main.cpp
@@ -7,9 +7,19 @@ int main()
     int income;
     cout << "Skolko ti zarabativaesh?" << endl;
     cin >> income;
-    if (income >1000) if(income < 1000000)cout <<  "Tak derzhat!";
-    if (income > 999999) cout << "Ti millioner!";
-    if (income < 1000) cout << "Rabotai bolshe";
-    cout << ".. no ti molodec!";
+    if (income > 1000)
+    {
+        if(income>1000000){
+          cout << "Ti millioner!";
+        }
+        if(income < 1000001){
+            cout <<  "Tak derzhat!";
+        }
+    }
+    if(income<1000)
+    {
+        cout << "Rabotai bolshe!";
+    }
+    cout << "\n.. no ti molodec!";
     return 0;
 }
