90106c1ea65d705f4fb35a7cc1a94a8255423d5f
Sirjeleshka <sirjeleshka@gmail.com>
2020-11-15 17:59:23 +0200

    Added #8

--- NG_2020_task8/main.cpp
+++ NG_2020_task8/main.cpp
@@ -10,16 +10,10 @@ int main()
     for(z=0;z<x;z++)
     {
         for(y=1;y<x-z;y++) cout << " ";
-        for(y=0;y<=z;y++) cout << '*';
-        for(y=0;y<z;y++)cout <<'*';
+        for(int y=0;y<=2*z;y++) cout << '*';
         cout << endl;
     }
-    for(z=0;z<=x;z++)
-    {
-            if(z==x-1) cout << '*';
-            else cout << " ";
-    }
-    cout << endl;
-
+    for(z=0;z<x-1;z++) cout << " ";
+    cout << '*' << endl;
     return 0;
 }
