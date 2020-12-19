89c04d132794206e4daf445fa4c535f4d73641b9
Sirjeleshka <sirjeleshka@gmail.com>
2020-12-19 17:34:00 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_1/main.cpp
+++ Lesson_2/NG_2020_task_1/main.cpp
@@ -6,23 +6,27 @@ int main()
 {
     int nomer_shkoli,moya_shkola;
     int mas[10];
-    for(int iter=0;iter<10;iter++)
+    for(int iter = 0;iter < 10;iter++)
     {
        cout << "vvedi nomer shkoli:";
        cin >> nomer_shkoli;
-       mas[iter]=nomer_shkoli;
-       if(nomer_shkoli<1) exit(0);
+       mas[iter] = nomer_shkoli;
+       if(nomer_shkoli < 1)
+       {
+           cout << "Oshibka!Neverno vvedeniy nomer shkoli.\n";
+           iter -= 1;
+       }
     }
     cout << "a teper vvedi svoyu shkolu:";
     cin >> moya_shkola;
-    for(int iter=0;iter<10;iter++)
+    for(int iter = 0;iter < 10;iter++)
     {
-        if(mas[iter]==moya_shkola)
+        if(mas[iter] == moya_shkola)
         {
             cout << "ya znayu etu shkolu!";
-            break;
+            nomer_shkoli = 1;
         }
-        else if(iter==9) cout << "ya ne znayu etu shkolu!";
     }
+    if(nomer_shkoli != 1) cout << "ya ne znayu etu shkolu!";
     return 0;
 }
--- Lesson_2/NG_2020_task_2/main.cpp
+++ Lesson_2/NG_2020_task_2/main.cpp
@@ -4,19 +4,30 @@ using namespace std;
 
 int main()
 {
+    int karti[10]={0};
     while(true)
     {
-       int karti[10]={0};
-       int nomer_karti,sum;
+       int nomer_karti,sum,all_cards_value = 0;
        cout << endl << "vvedite nomer kartochki:";
        cin >> nomer_karti;
-       if(nomer_karti>10||nomer_karti<0) break;
+       if(nomer_karti>10||nomer_karti<0)
+       {
+           continue;
+       }
        cout << "skolko_polozhit?";
        cin >> sum;
-       if(sum<0) break;
+       if(sum<0)
+       {
+           continue;
+       }
        cout << endl;
-       karti[nomer_karti] = sum;
-       for(int i=0;i<10;i++) cout << karti[i] << " ";
+       karti[nomer_karti] += sum;
+       for(int i=0;i<10;i++)
+       {
+           all_cards_value+=karti[i];
+           cout << karti[i] << " ";
+       }
+       cout << endl << "v summe na vseh kartah:" << all_cards_value;
     }
     return 0;
 }
