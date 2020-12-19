e8c1441e7d34bf0859751f313ec9b60aea33dee8
Sirjeleshka <sirjeleshka@gmail.com>
2020-12-19 17:50:57 +0200

    Added Lesson_3

---
+++ Lesson_3/NG_2020_task_2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char stroka[50];
+    int long_word = 0,long_word_next=1,position = 0, iter,false_sym;
+    cout << "Enter string: ";
+    cin.getline(stroka,50);
+    for(iter = 0;stroka[iter] != '\0';iter++)
+    {
+        if((stroka[iter]>='a'&&stroka[iter]<='z')||(stroka[iter]>='A'&&stroka[iter]<='Z'))
+        {
+            long_word_next++;
+            false_sym=0;
+            if(long_word<long_word_next) long_word = long_word_next;
+        }
+        else
+        {
+            long_word_next = 0;
+            false_sym++;
+        }
+    }
+    position = iter - long_word - false_sym;
+    cout << "Result: ";
+    for(iter = 0; iter<long_word;iter++) cout << stroka[position+iter];
+    return 0;
+}
