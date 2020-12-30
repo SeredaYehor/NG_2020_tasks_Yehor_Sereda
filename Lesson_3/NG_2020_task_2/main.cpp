6fb2db79c83eb1c4244721510ad59aa5e124d151
Sirjeleshka <sirjeleshka@gmail.com>
2020-12-30 13:06:16 +0200

    Added Lesson_3

--- Lesson_3/NG_2020_task_2/main.cpp
+++ Lesson_3/NG_2020_task_2/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     char stroka[50];
     int long_word = 0,long_word_next=1,position = 0, iter,false_sym;
-    cout << "Enter string: ";
+    cout << "Enter string:";
     cin.getline(stroka,50);
     for(iter = 0;stroka[iter] != '\0';iter++)
     {
@@ -14,7 +14,11 @@ int main()
         {
             long_word_next++;
             false_sym=0;
-            if(long_word<long_word_next) long_word = long_word_next;
+            if(long_word<long_word_next)
+            {
+                long_word = long_word_next;
+                position=iter+1-long_word;
+            }
         }
         else
         {
@@ -22,8 +26,10 @@ int main()
             false_sym++;
         }
     }
-    position = iter - long_word - false_sym;
-    cout << "Result: ";
-    for(iter = 0; iter<long_word;iter++) cout << stroka[position+iter];
+    cout << "Result:";
+    for(iter = 0; iter<long_word;iter++)
+    {
+        if(position+iter>=0)cout << stroka[position+iter];
+    }
     return 0;
 }
