813cf6029159f7f610ab1a2111adde7fd31eefb2
Sirjeleshka <sirjeleshka@gmail.com>
2020-12-30 11:13:42 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_1/main.cpp
+++ Lesson_2/NG_2020_task_1/main.cpp
@@ -4,17 +4,28 @@ using namespace std;
 
 int main()
 {
-    int nomer_shkoli,moya_shkola;
+    int moya_shkola;
+    float nomer_shkoli;
     int mas[10];
     for(int iter = 0;iter < 10;iter++)
     {
        cout << "vvedi nomer shkoli:";
        cin >> nomer_shkoli;
+       moya_shkola=nomer_shkoli;
        mas[iter] = nomer_shkoli;
-       if(nomer_shkoli < 1)
+       if(nomer_shkoli < 1||nomer_shkoli!=moya_shkola)
        {
            cout << "Oshibka!Neverno vvedeniy nomer shkoli.\n";
            iter -= 1;
+       }
+       for(int searching = iter-1;searching>=0;searching--)
+       {
+           if(nomer_shkoli==mas[searching])
+           {
+               cout << "Etot nomer uze prisutstvuet. Pogaluista vvedite drugoin nomer\n";
+               iter--;
+               break;
+           }
        }
     }
     cout << "a teper vvedi svoyu shkolu:";
--- Lesson_2/NG_2020_task_4/main.cpp
+++ Lesson_2/NG_2020_task_4/main.cpp
@@ -10,12 +10,12 @@ int main()
     int number_of_stolbik;
     for(number_of_stolbik=0;number_of_stolbik<5;number_of_stolbik++)
     {
-        cout << "zvezdochek v " << number_of_stolbik+1 << " stolbike:";
+        cout << "zvezdochek v " << number_of_stolbik+1 << " stolbike: ";
         cin >> stars;
         if(stars<0)
         {
-            cout << "Error:Unacceptable key";
-            exit(0);
+            cout << "Oshibka! Pojaluista, vvedite pravilnoe chislo.\n";
+            number_of_stolbik--;
         }
         arr[number_of_stolbik]=stars;
         if(max_stars<stars)max_stars = stars;
