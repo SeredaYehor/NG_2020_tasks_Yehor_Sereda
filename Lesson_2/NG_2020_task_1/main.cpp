a229a0f65a70487cedb09003cb2c2d71de49b539
Sirjeleshka <sirjeleshka@gmail.com>
2021-01-08 15:09:41 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_1/main.cpp
+++ Lesson_2/NG_2020_task_1/main.cpp
@@ -1,32 +1,19 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    int moya_shkola;
-    float nomer_shkoli;
-    int mas[10];
+    string output = "ya ne znayu etu shkolu!"; // question about string!!!
+    unsigned short int moya_shkola;
+    unsigned short int nomer_shkoli;
+    unsigned short int mas[10];
     for(int iter = 0;iter < 10;iter++)
     {
        cout << "vvedi nomer shkoli:";
        cin >> nomer_shkoli;
-       moya_shkola=nomer_shkoli;
        mas[iter] = nomer_shkoli;
-       if(nomer_shkoli < 1||nomer_shkoli!=moya_shkola)
-       {
-           cout << "Oshibka!Neverno vvedeniy nomer shkoli.\n";
-           iter -= 1;
-       }
-       for(int searching = iter-1;searching>=0;searching--)
-       {
-           if(nomer_shkoli==mas[searching])
-           {
-               cout << "Etot nomer uze prisutstvuet. Pogaluista vvedite drugoin nomer\n";
-               iter--;
-               break;
-           }
-       }
     }
     cout << "a teper vvedi svoyu shkolu:";
     cin >> moya_shkola;
@@ -34,10 +21,10 @@ int main()
     {
         if(mas[iter] == moya_shkola)
         {
-            cout << "ya znayu etu shkolu!";
-            nomer_shkoli = 1;
+            output = "ya znayu etu shkolu!";
+            iter = 10;
         }
     }
-    if(nomer_shkoli != 1) cout << "ya ne znayu etu shkolu!";
+    cout << output;
     return 0;
 }
