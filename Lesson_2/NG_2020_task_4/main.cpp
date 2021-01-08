21b5ec75f0e82766bc04c13edc718ab1ed9d8a53
Sirjeleshka <sirjeleshka@gmail.com>
2021-01-08 15:08:49 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_4/main.cpp
+++ Lesson_2/NG_2020_task_4/main.cpp
@@ -8,26 +8,26 @@ int main()
     int stars;
     int max_stars=0;
     int number_of_stolbik;
-    for(number_of_stolbik=0;number_of_stolbik<5;number_of_stolbik++)
+    for(number_of_stolbik = 0;number_of_stolbik < 5;number_of_stolbik++)
     {
         cout << "zvezdochek v " << number_of_stolbik+1 << " stolbike: ";
         cin >> stars;
-        if(stars<0)
+        if(stars < 0)
         {
             cout << "Oshibka! Pojaluista, vvedite pravilnoe chislo.\n";
             number_of_stolbik--;
         }
         arr[number_of_stolbik]=stars;
-        if(max_stars<stars)max_stars = stars;
+        if(max_stars < stars)max_stars = stars;
     }
-    for(int line = 0;line<=max_stars;line++)
+    for(int line = 0;line <= max_stars;line++)                  // fixed code style
     {
-    for(number_of_stolbik=0;number_of_stolbik<5;number_of_stolbik++)
-    {
-        if(arr[number_of_stolbik]-line>0)cout << '*';
-        else cout << " ";
-    }
-    cout<<endl;
+        for(number_of_stolbik = 0;number_of_stolbik < 5;number_of_stolbik++)
+        {
+            if(arr[number_of_stolbik] - line > 0) cout << '*';
+            else cout << " ";
+        }                                                       //
+    cout << endl;
     }
     return 0;
 }
