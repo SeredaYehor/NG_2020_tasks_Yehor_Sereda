a9a890835bd3e157444b2c114de9dba6f83adc71
Sirjeleshka <sirjeleshka@gmail.com>
2021-01-20 22:32:53 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_4/main.cpp
+++ Lesson_2/NG_2020_task_4/main.cpp
@@ -4,29 +4,26 @@ using namespace std;
 
 int main()
 {
-    int arr[5];
-    int stars;
-    int max_stars=0;
-    int number_of_stolbik;
-    for(number_of_stolbik = 0;number_of_stolbik < 5;number_of_stolbik++)
+    int arr[5], stars, max_stars = 0;
+    for(int number_of_stolbik = 0; number_of_stolbik < 5; number_of_stolbik++)
     {
-        cout << "zvezdochek v " << number_of_stolbik+1 << " stolbike: ";
+        cout << "zvezdochek v " << number_of_stolbik + 1 << " stolbike: ";
         cin >> stars;
         if(stars < 0)
         {
             cout << "Oshibka! Pojaluista, vvedite pravilnoe chislo.\n";
             number_of_stolbik--;
         }
-        arr[number_of_stolbik]=stars;
-        if(max_stars < stars)max_stars = stars;
+        arr[number_of_stolbik] = stars;
+        if(max_stars < stars) max_stars = stars;
     }
-    for(int line = 0;line <= max_stars;line++)                  // fixed code style
+    for(int line = 0;line <= max_stars;line++)
     {
-        for(number_of_stolbik = 0;number_of_stolbik < 5;number_of_stolbik++)
+        for(int number_of_stolbik = 0; number_of_stolbik < 5; number_of_stolbik++)
         {
             if(arr[number_of_stolbik] - line > 0) cout << '*';
             else cout << " ";
-        }                                                       //
+        }
     cout << endl;
     }
     return 0;
