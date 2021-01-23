7afa5d90598993f6c0759090c3d8a38d96d745c3
Sirjeleshka <sirjeleshka@gmail.com>
2021-01-23 12:34:24 +0200

    Added task_2

--- Lesson_6/NG_2020_task_2/textredactor.cpp
+++ Lesson_6/NG_2020_task_2/textredactor.cpp
@@ -9,9 +9,9 @@ TextRedactor::TextRedactor(QWidget *parent)
 {
     ui->setupUi(this);
 
-    ui->spinBox->setReadOnly(true);
+    ui->Num_of_words->setReadOnly(true);
 
-    connect (ui->textEdit, &QTextEdit::textChanged, this, &TextRedactor::output);
+    connect (ui->Input_text, &QTextEdit::textChanged, this, &TextRedactor::output);
 }
 
 TextRedactor::~TextRedactor()
@@ -21,9 +21,9 @@ TextRedactor::~TextRedactor()
 
 void TextRedactor::output()
 {
-    QString input =  ui->textEdit->toPlainText();
+    QString input =  ui->Input_text->toPlainText();
     int i = 0,b = 0;
-    ui->spinBox->setValue(0);
+    ui->Num_of_words->setValue(0);
     if(input[0].isLetter()) b = 1;
     while(i < input.length()-1)
     {
@@ -31,7 +31,7 @@ void TextRedactor::output()
         {
             b+=1;
         }
-        ui->spinBox->setValue(b);
+        ui->Num_of_words->setValue(b);
         i++;
     }
 }
