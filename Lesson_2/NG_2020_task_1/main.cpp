9c67f7b64cbae314db45872af4c9658fa3446c87
Sirjeleshka <sirjeleshka@gmail.com>
2021-01-23 12:02:40 +0200

    Added Lesson_2

--- Lesson_2/NG_2020_task_1/main.cpp
+++ Lesson_2/NG_2020_task_1/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main()
 {
-    unsigned short int moya_shkola, nomer_shkoli, mas[10];
-    for(int iter = 0;iter < 10;iter++)
+    unsigned short int moya_shkola, nomer_shkoli, mas[10], iter;
+    for(iter = 0;iter < 10;iter++)
     {
        cout << "vvedi nomer shkoli:";
        cin >> nomer_shkoli;
@@ -13,14 +13,9 @@ int main()
     }
     cout << "a teper vvedi svoyu shkolu:";
     cin >> moya_shkola;
-    for(int iter = 0;iter < 10;iter++)
-    {
-        if(mas[iter] == moya_shkola)
-        {
-            cout << "ya znayu etu shkolu!";
-            return 0;
-        }
-    }
-    cout << "ya ne znau etu shkolu!";
+    iter = 0;
+    while(mas[iter]!=moya_shkola&&iter<=10){iter++;}
+    if(iter<10) cout << "ya znayu etu shkolu!";
+    else cout << "ya ne znau etu shkolu!";
     return 0;
 }
